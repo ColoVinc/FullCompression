2bc9f4654685a40e20680a7df86d8de144d2a088
ColoVinc <vincolonna1@gmail.com>
2025-07-06 16:18:58 +0200

    correzioni

--- wp-content/plugins/full-compression/full-compression.php
+++ wp-content/plugins/full-compression/full-compression.php
@@ -4,7 +4,7 @@
  * Description: Comprimi e converti automaticamente le immagini caricate su WordPress.
  * Version: 1.0.0
  * Author: Vincenzo Colonna
- * Text Domain: full-compression
+ * Text Domain: fullcompression
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -16,7 +16,3 @@ if (!defined('ABSPATH')) {
 
 require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
 require_once plugin_dir_path(__FILE__) . 'includes/image-processor.php';
-
-add_action('plugins_loaded', function () {
-    load_plugin_textdomain('full-compression', false, dirname(plugin_basename(__FILE__)) . '/languages');
-});
